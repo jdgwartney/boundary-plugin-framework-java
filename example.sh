f0b967c3aa49dcfbcce84edcf59f7b4b34bace3d
Gwartney <davidg@boundary.com>
2016-02-12 13:23:36 -0800

    Add argument for JMX listener port

--- example.sh
+++ example.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-java -cp target/test-classes -Dcom.sun.management.jmxremote.port=9991 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false com.boundary.plugin.sdk.jmx.ExampleAgent
+if [ $# -ne 1 ]
+then
+   echo "usage: $(basename $0) jmx_port"
+   echo "where:"
+   echo "    jmx_port - Listening port of JMX"
+   exit 1
+fi
+typeset -r JMX_PORT=$1
+exec java -cp target/test-classes -Dcom.sun.management.jmxremote.port="$JMX_PORT" -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false com.boundary.plugin.sdk.jmx.ExampleAgent
